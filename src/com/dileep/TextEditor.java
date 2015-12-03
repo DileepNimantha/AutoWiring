8498f591ea553262135eac6d6a17f029de3124a5
DileepNimantha <dileepnimantha@gmail.com>
2015-12-03 17:31:01 +0530

    Demo auto wiring

---
+++ src/com/dileep/TextEditor.java
@@ -0,0 +1,27 @@
+package com.dileep;
+
+public class TextEditor {
+	private SpellChecker spellChecker;
+	private String name;
+
+	public SpellChecker getSpellChecker() {
+		return spellChecker;
+	}
+
+	public void setSpellChecker(SpellChecker spellChecker) {
+		this.spellChecker = spellChecker;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		System.out.println(name);
+	}
+
+	public void spellCheck() {
+		spellChecker.checkSpelling();
+	}
+}
